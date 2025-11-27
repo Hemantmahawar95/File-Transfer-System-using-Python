d9f3d49635da4ad17241089292f22182f029c5ce
Hemant <hemantmahawar278@gmail.com>
2025-11-27 21:02:37 +0530

    Implement basic HTTP server in File_Transfer.py

---
+++ File_Transfer.py
@@ -0,0 +1,12 @@
+import http.server
+import socketserver  
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Server running at http://localhost:8000",PORT)
+    httpd.serve_forever()
+
+#ipconfig
